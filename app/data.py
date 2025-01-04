09f2bf2696eb2f8314a779a42d366fc366b73b1c
Russell Salazar <83458251+Russell0308@users.noreply.github.com>
2025-01-04 17:26:00 -0500

    complete

--- app/data.py
+++ app/data.py
@@ -11,33 +11,53 @@ from pymongo.server_api import ServerApi
 class Database:
 
     def __init__(self):
+        '''
+        Initializes connection to Mongo Atlas
+        Sets database and collection variable
+        Tests connection to Mongo Atlas remote server
+        '''
         load_dotenv()
         self.client = MongoClient(getenv("DB_URL"), tls=True, server_api=ServerApi("1"))
         self.db = self.client['database']
         self.collection = self.db['monster_collection']
-        try: 
-            self.client.admin.command('ping')
-            print('Success!')
 
-        except Exception as e:  
-            print(e)
-        
 
     def seed(self, amount):
+        '''
+        Adds a variable amount of documents(Monsters) to the database collection
+        '''
         for i in range(amount):
             document = Monster().to_dict()
             self.collection.insert_one(document)
         
     def reset(self):
+        '''
+        Empties the "Monsters collection"
+        '''
         deleted = self.collection.delete_many({})
 
         return deleted.deleted_count
 
     def count(self) -> int:
+        '''
+        Returns the amount of documents(Monsters) in the "Monsters collection"
+        '''
         return self.collection.count()
 
     def dataframe(self) -> DataFrame:
-        pass
+        '''
+        Puts documents(Monsters) into a dataframe
+        '''
+        documents = self.collection.find()
+        df = DataFrame(list(documents))
+
+        return df
 
     def html_table(self) -> str:
-        pass
+        '''
+        Makes the dataframe web ready by changing it to an html table
+        '''
+        if self.dataframe().empty:
+            return None
+        else: 
+            return self.dataframe().drop('_id', axis=1).to_html()
